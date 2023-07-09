cfefd5a0867c359b84590d689b80269ddadd7cec
unknown <piratelo.paulo@gmail.com>
2023-07-09 13:39:52 -0300

    added file image_downloader.py to dwld images using googleimages

--- image_downloader.py
+++ image_downloader.py
@@ -1,8 +1,22 @@
 from bing_image_downloader import downloader
+import pandas as pd
+import openpyxl
 
-query_string = ["stegosauria", "ankylosauria", "ornithopoda"]
-limit_images = 20
+
+path_excel = "C:/Users/Paulo/Documents/dino_expert/dinosauria_tree.xlsx"
+
+my_classes_df = pd.read_excel(path_excel, usecols='D')
+
+my_classes_df.dropna(how="any", inplace=True)
+my_classes = my_classes_df.values.tolist()
+
+
+limit_images = 2
 output_images = 'dataset_dinosauria'
 
-csv_classes = ""
-downloader.download(query_string, limit=limit_images,  output_dir=output_images, adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+for class_ in my_classes:
+    #print(type(class_))
+    class_ = (  str(class_)[2:-2]  )
+    aviso = f'Começando o download da classe {class_}:'
+    print(aviso)
+    downloader.download(class_, limit=limit_images,  output_dir=output_images, adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
